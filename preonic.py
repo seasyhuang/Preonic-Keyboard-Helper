4df2d65da31d50e6b52fb4e27205d29a08e7fea4
Seasy <seasy.huang@gmail.com>
2018-02-14 16:28:57 -0500

    Incorporated additional functionality except clearly I don't understand how dictionaries work in python

--- preonic.py
+++ preonic.py
@@ -1,5 +1,43 @@
-layout = []
-layout.extend(["Preonic Qwerty Layout", ",-----------------------------------------------------------------------------------. ", "|   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp | ", "|------+------+------+------+------+------+------+------+------+------+------+------| ", "| Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Del  | ", "|------+------+------+------+------+-------------+------+------+------+------+------| ", "| Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  \   | ", "|------+------+------+------+------+------|------+------+------+------+------+------| ", "| Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter | ", "|------+------+------+------+------+------+------+------+------+------+------+------| ", "| Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right | ", ".-----------------------------------------------------------------------------------. "])
+def main():
+    cmd = raw_input('''Possible commands: 
+    l - print keyboard layout
+    s - common shortcuts that you keep forgetting
+    m - randomly generated motivational message
+    ----------------------------------------
+    Please enter a command:
+    >>> ''')
+    type(cmd)
+    
+#     choices = {
+#         'l': layout(),
+#         'm': randomMessage() 
+#         }
+#     result = choices.get(cmd, "wrong")
+    
+def layout():
+    layout = '''Preonic Qwerty Layout
+    |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |  
+    |------+------+------+------+------+------+------+------+------+------+------+------|  
+    | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Del  | 
+    |------+------+------+------+------+-------------+------+------+------+------+------| 
+    | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  \   | 
+    |------+------+------+------+------+------|------+------+------+------+------+------| 
+    | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter | 
+    |------+------+------+------+------+------+------+------+------+------+------+------| 
+    | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+    .-----------------------------------------------------------------------------------.
+    
+    '''
+    print layout
+    
+def randomMessage():
+    randomMessage = "c's get degrees"
+    print randomMessage
 
-for line in layout:
-    print line
+def shortcuts():
+    print ("Run code (eclipse): CTRL + LOWER + B // CTRL + F11")
+    
+# -----------------------
+
+if __name__ == "__main__":
+    main()
